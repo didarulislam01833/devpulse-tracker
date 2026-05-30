71437aad4e99154789b3d22e3f3d024f0d662571
didarulislam01833 <didarulislam018333@gmail.com>
2026-05-31 02:15:13 +0600

    Just Updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,17 +8,15 @@ const config = {
     jwt_secret: process.env.JWT_SECRET as string,
 };
 
-
 if (!config.jwt_secret) {
-    console.error("FATAL: JWT_SECRET is not defined in environment variables!");
+    console.log("fatal: JWT_SECRET is not defined in enviroment variable!");
     process.exit(1);
 }
+
 
 if (!config.connection_string) {
-    console.error("FATAL: DATABASE_URL is not defined in environment variables!");
+    console.error("fatal: DATABASE_URL is not defined in enviroment variable!");
     process.exit(1);
 }
-
-console.log("Config loaded successfully");
 
 export default config;
