2e3fb36d78d8af6d70b407c838e1fbeb984d6e6d
didarulislam01833 <didarulislam018333@gmail.com>
2026-05-30 23:34:54 +0600

    feat(auth): implement JWT verification and role-based authorization middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,58 @@
+import type { NextFunction, Request, Response } from "express"
+import jwt, { type JwtPayload, type Jwt } from "jsonwebtoken";
+import config from "../config";
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: JwtPayload & { id: number; name: string; role: string };
+        }
+    }
+}
+
+const authMiddleware = (...requireRoles: string[]) => {
+    return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const authHeader = req.headers.authorization;
+
+            if (!authHeader || !authHeader.startsWith("Bearer")) {
+                res.status(401).json({
+                    success: false,
+                    message: "unauthorized",
+                    errors: "you are not authorized to access this route, token missing!"
+                });
+                return;
+            }
+            const token = authHeader.split(" ")[1];
+
+            // const decode = jwt.verify(token, process.env.JWT_Acces_Secret || "secret-key") as JwtPayload & { id: number; name: string ; role: string };
+            // const decode = jwt.verify(token as string, (process.env.JWT_Acces_Secret || "secret=key")) as unknown as JwtPayload & { id: number; name: string; role: string }
+
+            const decode = jwt.verify(
+                token as string,
+                config.jwt_secret as string
+
+            ) as unknown as JwtPayload & { id: number; name: string; role: string };
+
+            if (requireRoles.length > 0 && !requireRoles.includes(decode.role)) {
+                res.status(403).json({
+                    success: false,
+                    message: "forbidden",
+                    errors: "you do not have permission to perform this action"
+                });
+                return;
+            }
+            req.user = decode;
+            next();
+
+
+        } catch (error: any) {
+            res.status(401).json({
+                success: false,
+                message: "unauthorized",
+                errors: error.message || "Invalid or expired token !"
+            });
+        }
+    };
+};
+export { authMiddleware };
